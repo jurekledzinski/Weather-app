eecae28fba5d0ff91073e37ab0a2ec66f7d8c96e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-05-31 21:51:41 +0200

    Add chart.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+const labels = ["January", "February", "March", "April", "May", "June"];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 0)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30],
+    },
+  ],
+};
 
+const config = {
+  type: "line",
+  data,
+  options: {},
+};
+
+var myChart = new Chart(document.getElementById("myChart"), config);
