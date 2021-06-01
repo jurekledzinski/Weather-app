c9626ba6fd8d4ac2e9158ecf198464318e548429
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-01 10:13:22 +0200

    Customize appearance of chart

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = ["2 p.m.", "3 p.m.", "4 p.m.", "5 p.m.", "6 p.m.", "7 p.m."];
 const data = {
   labels: labels,
   datasets: [
     {
-      label: "My First dataset",
+      label: "Temperatures",
       backgroundColor: "rgb(255, 99, 0)",
       borderColor: "rgb(255, 99, 132)",
       data: [0, 10, 5, 2, 20, 30],
+      cubicInterpolationMode: "monotone",
     },
   ],
 };
@@ -14,7 +15,17 @@ const data = {
 const config = {
   type: "line",
   data,
-  options: {},
+  options: {
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          color: "rgb(95,95,95)",
+        },
+      },
+    },
+  },
 };
 
 var myChart = new Chart(document.getElementById("myChart"), config);
