da9115f77824f60c10acf818c04c585badfa0d9f
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-01 22:52:10 +0200

    Add functionality to show hide details weather on mobile devices when click buttons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+// const sixDaysForecast = document.querySelector(
+//   ".slider__box-4-boxes--days-forecast"
+// );
+// const hourlyWeather = document.querySelector(
+//   ".slider__box-4-boxes--hourly-weather"
+// );
+
+const detailsTitle = document.querySelector(".slider__box-4-title");
+
+const detailsWeather = document.querySelectorAll(
+  ".slider__box-4-boxes,.slider__box-4-boxes--days-forecast,.slider__box-4-boxes--hourly-weather"
+);
+
+const innerBoxFour = document.querySelector(".slider__inner-box-4");
+
 const buttonsButtom = document.querySelectorAll(".slider__bottom-btn");
 
 const handleContentLoaded = () => {
-  const firstButtonCheckWeather = buttonsButtom[0];
-  firstButtonCheckWeather.classList.add("active-btn-bottom");
+  const firstDetailsWeather = detailsWeather[0];
+  const firstButtonCheckWeatherDetails = buttonsButtom[0];
+  firstButtonCheckWeatherDetails.classList.add("active-btn-bottom");
+  firstDetailsWeather.classList.add("details-visible");
 
   const labels = ["2 p.m.", "3 p.m.", "4 p.m.", "5 p.m.", "6 p.m.", "7 p.m."];
 
@@ -37,18 +54,52 @@ const handleContentLoaded = () => {
 
   var myChart = new Chart(document.getElementById("myChart"), config);
 
-  const handleClickButtonCheckWeather = (e) => {
+  const handleClickButtonCheckWeather = (e, indexBtn) => {
     buttonsButtom.forEach((item) => {
       item.classList.remove("active-btn-bottom");
     });
 
     const btn = e.target;
     btn.classList.add("active-btn-bottom");
+
+    innerBoxFour.style.transform = `translateX(-${indexBtn}00%)`;
+
+    detailsWeather.forEach((item, index) => {
+      item.classList.remove("details-visible");
+
+      if (index === indexBtn) {
+        const sliderBox4 = document.querySelectorAll(".slider__box-4");
+        detailsTitle.textContent = btn.innerHTML;
+        item.classList.add("details-visible");
+        console.log(sliderBox4);
+      }
+    });
   };
 
-  buttonsButtom.forEach((item) => {
-    item.addEventListener("click", (e) => handleClickButtonCheckWeather(e));
+  buttonsButtom.forEach((item, index) => {
+    item.addEventListener("click", (e) =>
+      handleClickButtonCheckWeather(e, index)
+    );
   });
+
+  const CheckSizeWindow = () => {
+    if (window.innerWidth >= 768) {
+      buttonsButtom.forEach((item) => {
+        item.classList.remove("active-btn-bottom");
+      });
+
+      detailsWeather.forEach((item, index) => {
+        item.classList.remove("details-visible");
+      });
+
+      firstDetailsWeather.classList.add("details-visible");
+      innerBoxFour.style.transform = `translateX(0%)`;
+      firstButtonCheckWeatherDetails.classList.add("active-btn-bottom");
+      detailsTitle.textContent = "Details";
+    }
+  };
+
+  window.addEventListener("resize", CheckSizeWindow);
 };
 
 window.addEventListener("DOMContentLoaded", handleContentLoaded);
