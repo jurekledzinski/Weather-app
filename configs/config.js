b0e867b6276bc660f1140e31dacfdd67197b8df3
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-06-07 08:59:46 +0200

    Create config.js file and add references to environmental variables

---
+++ configs/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  openWeatherApiKeyPart1: process.env.OPEN_WEATHER_API_KEY_PART_1,
+  openWeatherApiKeyPart2: process.env.OPEN_WEATHER_API_KEY_PART_2,
+  geoLocationApiKey: process.env.GEO_LOCATION_API_KEY,
+};
